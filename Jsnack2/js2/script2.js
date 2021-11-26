36a0e1f3f8880c311f8b5709f2870ba0d1962b44
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-11-26 17:35:47 +0100

    script2 non finito

--- Jsnack2/js2/script2.js
+++ Jsnack2/js2/script2.js
@@ -11,15 +11,65 @@ console.log(guests);
 //chiedo all'utente il suo nome
 
 let username = prompt("Come ti chiami?")
+console.log(username);
 
 //verifico SE appartiene alla mia lista
-const container = document.querySelector (".container");
 
-if (guests.includes(username)) {
-    container.innerHTML = ("Puoi entrare");
+//ciclo while
+
+// i = 0;
+
+// while (i < guests.length) {
+
+//     console.log("ciao");
+//     if (username == guests [i]) {
+//          alert ("porcodio");
+//          i ++;
+//     } else {
+//          alert ("Muori oggi");
+//     }
+// }
+
+
+//NON FINITO.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ciclo for
+
+
+let found = false;
+
+let message = "Non puoi entrare"
+
+for (let i = 0; i < guests.length; i++) {
     
-} else {
-    container.innerHTML = ("Non puoi entrare");
-}
+    if (username == guests[i]) {
+        found = true;
+        console.log(found);        
+    } 
 
-//voglio che il prompt accetti anche le lettere minuscole
+    if (found == true) {
+
+        const container = document.querySelector (".container");
+        container.innerHTML = "PUOI ENTRARE";
+
+    } else {
+        const container = document.querySelector (".container");
+        container.innerHTML = "NON PUOI ENTRARE";
+    }
+
+}    
+
